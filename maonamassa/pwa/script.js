42beb8e03ecaf72a3c3f7be09ba2dbe5fc3c850d
Bruno Panerai Velloso <bruno.velloso@ifsc.edu.br>
2025-09-17 20:12:48 -0300

    aplicação PWA

--- maonamassa/pwa/script.js
+++ maonamassa/pwa/script.js
@@ -118,3 +118,19 @@ async function abrirDetalhes(id) {
       console.error("Erro ao carregar detalhes:", error)
   }
 }
+
+if ('serviceWorker' in navigator) {  
+    navigator.serviceWorker.register("./service-worker.js");
+}
+
+var pedidoInstalacao;
+window.addEventListener('beforeinstallprompt', function(installPrompt) {
+    if(installPrompt){
+        document.getElementById("installAppBt").classList.add('show')
+        pedidoInstalacao = installPrompt
+    }
+});
+
+function installApp() {
+    pedidoInstalacao.prompt(); 
+}
